3ae4c64cf5523e07166c78c10fd97512000053b1
warren-west <teacher.warren.english@gmail.com>
2024-10-15 10:12:05 +0200

    Iterating over properties of an object, and accessing the values.

---
+++ iteratingObjects.js
@@ -0,0 +1,42 @@
+const myCar = {
+    brand: "Ferrari",
+    model: "458",
+    color: "red",
+    horsePower: 1200,
+    isConvertible: true,
+    isAutomatic: false,
+    // new prop
+}
+
+
+// DOM Elements
+function renderCarDetails() {
+    
+    // for, for...of, for...in
+    for (let key in myCar) {
+        console.log(key)
+        console.log(myCar[key])
+
+        const newPTag = document.createElement("p")
+        newPTag.id = "car-" + key
+        newPTag.innerText = `${key}: ${myCar[key]}`
+
+        document.body.appendChild(newPTag)
+    }
+    
+}
+
+// Runtime
+renderCarDetails()
+
+function forInLoopWithArrays() {
+    // We can also use for...in loops with arrays
+    // all it does is expose the element index to us as "key"
+    // here I've named the "key" as "idx", for index.
+    let superCars = ["Lamborghini", "Ferrari", "Porsche"]
+    
+    for (let idx in superCars) {
+        console.log(idx)
+        console.log(superCars[idx])
+    }
+}
